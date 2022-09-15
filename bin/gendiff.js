4a21e0a6d88ac81ad3f875e42b49db5eb8bc97ab
bmwmtv <bmwmtv@gmail.com>
2022-09-15 13:21:33 +0700

    второй шаг

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 
-const { program } = require('commander');
-//const program = new Command();
+
+import { Command } from 'commander';
+const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.1');
+  .version('0.1')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
 
   program.parse();
