e59060229ac6609ff04ee55f2895addb5f62cf19
bmwmtv <bmwmtv@gmail.com>
2022-09-16 19:32:02 +0700

    update

--- bin/parser.js
+++ bin/parser.js
@@ -4,34 +4,35 @@ import _ from 'lodash';
 const diff = (data1, data2) => {
   const keys = _.union(_.keys(data1), _.keys(data2)).sort();
 
-  const result = [];
-  result.push('{');
-  keys.map((key) => {
+  const text = keys.flatMap((key) => {
     // если равны ключи и значения
+    const temp = [];
     if (_.has(data1, key) && _.has(data2, key)) {
       if (data1[key] === data2[key]) {
-        result.push(`    ${key}: ${data1[key]}`);
+        temp.push(`    ${key}: ${data1[key]}`);
       }
     }
     // если равны ключи , но значения разные
     if (_.has(data1, key) && _.has(data2, key)) {
       if (data1[key] !== data2[key]) {
-        result.push(`  - ${key}: ${data1[key]}`);
-        result.push(`  + ${key}: ${data2[key]}`);
+        temp.push(`  - ${key}: ${data1[key]}`);
+        temp.push(`  + ${key}: ${data2[key]}`);
       }
     }
     // если ключ есть в первом файле но отсутствует во втором
     if (_.has(data1, key) && !_.has(data2, key)) {
-      result.push(`  - ${key}: ${data1[key]}`);
+      temp.push(`  - ${key}: ${data1[key]}`);
     }
 
     // если ключ есть во втором файле но отсутствует в первом
     if (!_.has(data1, key) && _.has(data2, key)) {
-      result.push(`  + ${key}: ${data2[key]}`);
+      temp.push(`  + ${key}: ${data2[key]}`);
     }
+    return temp;
   });
-  result.push('}');
-  return result.join('\n');
+
+  const result = _.concat('{', text, '}');
+  return _.join(result, '\n');
 };
 
 const parser = (filepath1, filepath2) => {
