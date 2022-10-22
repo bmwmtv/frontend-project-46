6606d73af014740defcb1f762f4c635894394095
bmwmtv <bmwmtv@gmail.com>
2022-10-22 14:24:21 +0700

    if to switch

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -15,23 +15,22 @@ const formatPlain = (diff, path = []) => {
   const output = filteredDiff.map((item) => {
     const newPath = path.concat(item.key);
     const node = newPath.join('.');
+    switch (item.state) {
+      case 'removed':
+        return `Property '${node}' was removed`;
 
-    if (item.state === 'removed') {
-      return `Property '${node}' was removed`;
+      case 'added': {
+        const val = prepareValue(item.value);
+        return `Property '${node}' was added with value: ${val}`; }
+
+      case 'updated': {
+        const oldVal = prepareValue(item.value.oldValue);
+        const newVal = prepareValue(item.value.newValue);
+        return `Property '${node}' was updated. From ${oldVal} to ${newVal}`; }
+
+      default:
+        return formatPlain(item.value, newPath);
     }
-
-    if (item.state === 'added') {
-      const val = prepareValue(item.value);
-      return `Property '${node}' was added with value: ${val}`;
-    }
-
-    if (item.state === 'updated') {
-      const oldVal = prepareValue(item.value.oldValue);
-      const newVal = prepareValue(item.value.newValue);
-      return `Property '${node}' was updated. From ${oldVal} to ${newVal}`;
-    }
-
-    return formatPlain(item.value, newPath);
   }).join('\n');
 
   return output;
